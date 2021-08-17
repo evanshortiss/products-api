5d09c6fad62c95894366b11a89fd4892a5cadb47
Evan Shortiss <evanshortiss@gmail.com>
2021-08-17 15:55:46 +0100

    feat: add 3scale proxy secret check

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ oc login --token=$TOKEN --server=$API_SERVER
 
 mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.openshift.expose=true
 ```
+
+## Enforce 3scale Proxy Token Security
+
+Set the `THREESCALE_SECRET_HEADER_VALUE` environment variable to the secret
+value you configured in the 3scale API Manager/Admin portal.
--- src/main/java/org/acme/products/LoggingFilter.java
+++ src/main/java/org/acme/products/LoggingFilter.java
@@ -1,14 +1,19 @@
 package org.acme.products;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +23,12 @@ import io.vertx.core.http.HttpServerRequest;
 public class LoggingFilter implements ContainerRequestFilter {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Inject
+    Config config;
+
+    @ConfigProperty(name = "api.security.header.name")
+    String header;
 
     @Context
     UriInfo info;
@@ -30,11 +41,28 @@ public class LoggingFilter implements ContainerRequestFilter {
         if (log.isDebugEnabled()) {
             final String method = context.getMethod();
             final String path = info.getPath();
-
             log.debug("Request for {} {}", method, path);
             log.debug("Request headers:");
             MultivaluedMap<String, String> rh = context.getHeaders();
             rh.forEach((key, value) -> log.debug("{} = {}", key, value));
+        }
+
+        Optional<String> secret = config.getOptionalValue("api.security.header.value", String.class);
+
+        if (secret.isPresent()) {
+            String incomingSecret = context.getHeaderString(header);
+            String desiredSecret = secret.get().toString();
+
+            log.info("enforcing " + header + " check using secret value: \"" + desiredSecret + "\"");
+
+            if (incomingSecret.equals(desiredSecret)) {
+                log.info("request passed security header check");
+            } else {
+                log.warn("request failed security header check. Incoming secret value was: \"" + incomingSecret + "\"");
+                context.abortWith(Response.status(401).entity("Unauthorized: Invalid " + header).build());
+            }
+        } else {
+            log.info("security header check is disabled");
         }
     }
 }
